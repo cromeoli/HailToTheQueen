63fb209fb6e00b0df948f4a3102644859bb5b0c5
cromeoli <cromero@iesrafaelalberti.es>
2023-03-02 03:54:01 +0100

    Ya es responsive... casi soy desarrollador ya y todo

--- src/assets/js/coolSlider.js
+++ src/assets/js/coolSlider.js
@@ -1,3 +1,13 @@
+const navbar = document.getElementById("navbar")
+const queenFloatingIcon = document.querySelector(".floatingMenuIcon");
+
+const menuIconNavbar = document.getElementById("navbarMenuIcon")
+const menu = document.querySelector('.menu')
+const homeText = document.getElementById('homeText')
+const charText = document.getElementById('charText')
+const worldText = document.getElementById('worldText')
+const extraText = document.getElementById('extraText')
+
 const knightAudio1 = new Audio("src/assets/audio/EvilKnightRoar.mp3")
 const knightAudio2 = new Audio("src/assets/audio/EvilKnightLaugh.mp3")
 
@@ -20,6 +30,29 @@ const observer = new IntersectionObserver((entries) => {
     })
 })
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time))
+}
+
+function openMenu(){
+    document.body.style.overflow = "hidden";
+    navbar.classList.add('navbarFixed')
+    menu.style.display = "flex"
+
+    delay(25).then(() => menu.style.height = "90vh")
+    delay(30).then(() => menu.style.opacity = 100)
+}
+
+function closeMenu(){
+    document.body.style.overflow = "auto";
+    let menu = document.querySelector('.menu')
+    navbar.classList.remove('navbarFixed')
+
+    menu.style.height = 0
+    delay(250).then(() => menu.style.opacity = 0)
+    delay(550).then(() => menu.style.display = "none")
+}
+
 const handleOnMove = e => {
     const p = e.clientX / window.innerWidth * 100;
 
@@ -28,12 +61,13 @@ const handleOnMove = e => {
 }
 
 
-hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements.forEach((element) => observer.observe(element))
 
 knightCard.addEventListener("click", function() {
     this.classList.toggle("flipped");
 
     if (this.classList.contains("flipped")) {
+        knightAudio1.volume = 0.1;
         knightAudio1.play();
     }
 });
@@ -42,9 +76,61 @@ pawnCard.addEventListener("click", function() {
     this.classList.toggle("flipped");
 
     if (this.classList.contains("flipped")) {
+        knightAudio2.volume = 0.1;
         knightAudio2.play();
     }
 });
 
+menuIconNavbar.addEventListener("click", function() {
+    const computedStyle = window.getComputedStyle(menu);
+    const displayValue = computedStyle.getPropertyValue("display");
+
+    if (displayValue === "none") {
+        openMenu();
+    } else {
+        closeMenu();
+    }
+});
+
+homeText.addEventListener("click", function() {
+    closeMenu()
+});
+
+charText.addEventListener("click", function() {
+    closeMenu()
+});
+
+worldText.addEventListener("click", function() {
+    closeMenu()
+});
+
+extraText.addEventListener("click", function() {
+    closeMenu()
+});
+
+
+
 sectionCoolSlider.addEventListener("mousemove", e => handleOnMove(e))
 sectionCoolSlider.addEventListener("touchmove", e => handleOnMove(e.touches[0]));
+
+queenFloatingIcon.addEventListener("click", function() {
+    const computedStyle = window.getComputedStyle(menu);
+    const displayValue = computedStyle.getPropertyValue("display");
+
+    if (displayValue === "none") {
+        openMenu();
+    } else {
+        closeMenu();
+    }
+});
+
+window.addEventListener("scroll", function() {
+    const scrollPosition = window.scrollY;
+    const navbarPosition = navbar.getBoundingClientRect().top;
+
+    if (navbarPosition < scrollPosition) {
+        queenFloatingIcon.style.display = "flex";
+    } else {
+        queenFloatingIcon.style.display = "none";
+    }
+});
